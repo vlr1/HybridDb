d1bd48a95c357cc1e6a1e6a1e7fb85ae96342b5c
Asger Hallas <ahl@asgerhallas.dk>
2020-11-06 10:07:33 +0100

    Remove debugging statements

--- src/HybridDb/DocumentStore.cs
+++ src/HybridDb/DocumentStore.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.ExceptionServices;
 using System.Text.RegularExpressions;
 using System.Threading;
 using Dapper;
